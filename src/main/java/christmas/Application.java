b7d8e71d24cd03e631e071c9c84e5143fc904808
PINGPINGYEE <owb7994@nate.com>
2023-11-15 14:13:22 +0900

    Docs: 문서 최종 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,19 +1,9 @@
 package christmas;
 
-import christmas.domain.OrderManager;
-import christmas.domain.OrderResult;
-import christmas.view.InputView;
-import christmas.view.OutputView;
-import christmas.domain.menuManage.OrderHistory;
-
-import java.util.List;
-
 import static christmas.domain.OrderManager.startOrder;
 
 public class Application {
     public static void main(String[] args) {
         startOrder();
     }
-
-
 }

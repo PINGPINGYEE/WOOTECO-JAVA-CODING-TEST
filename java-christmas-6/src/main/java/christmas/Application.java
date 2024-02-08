4635f4a446b85b26a007d9c210599e2ee1dbde89
PINGPINGYEE <owb7994@nate.com>
2024-02-08 14:09:48 +0900

    deleted all

--- java-christmas-6/src/main/java/christmas/Application.java
+++
@@ -1,9 +0,0 @@
-package christmas;
-
-import static christmas.domain.OrderManager.startOrder;
-
-public class Application {
-    public static void main(String[] args) {
-        startOrder();
-    }
-}
